3102b588c09a962ba8c151b9f4e31d913b334d79
FalkKi <nochkorzi@gmail.com>
2022-04-20 10:08:46 +0300

    Co-authored-by: hirurg-lybitel <hirurg-lybitel@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-$(document).ready(function () {
-   $('.icon-menu').click(function (event) {
-      $('.icon-menu').toggleClass('active');
-      $('.menu__body').toggleClass('active');
-      $('body').toggleClass('lock');
+(function () {
+   const header = document.querySelector('header');
+   window.onscroll = () => {
+      if (window.pageYOffset > 70) {
+         header.classList.add('header_active');
+      } else {
+         header.classList.remove('header_active');
+      }
+   };
+}());
+
+
+(function () {
+   const burgerItem = document.querySelector('.burger');
+   const menu = document.querySelector('.header__nav');
+   const menuCloseItem = document.querySelector('.header__nav_close')
+   burgerItem.addEventListener('click', () => {
+      menu.classList.add('header__nav-active');
    });
-});
+   menuCloseItem.addEventListener('click', () => {
+      menu.classList.remove('header__nav-active');
+   });
+
+}());
+
+
